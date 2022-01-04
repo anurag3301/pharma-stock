78d7e8ffbb38846ff3d40cfba7aab92d974fd026
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-01-04 21:09:31 +0530

    Update code for excel reading and writing using pandas

--- excel.py
+++ excel.py
@@ -1,7 +1,17 @@
-from openpyxl import Workbook, load_workbook
+import pandas as pd
+import os
+import string
+import random
+os.chdir("/home/anurag/repo/pharma-stock")
+data = pd.read_excel('data.xlsx', sheet_name='Sheet1')
 
-wb = load_workbook('data.xlsx')
+def rand_str(N):
+    return ''.join(random.choices(string.ascii_uppercase, k = N))
 
-ws = wb.active
+for i in range(10):
+    data = data.append({'med_name':rand_str(12), 'company':rand_str(12),\
+            'expiry_date':pd.to_datetime('2021-12-12'), 'inhand_stock':random.randint(1, 1000),\
+            'store_stock':random.randint(1, 1000), 'piece':random.randint(1, 1000),\
+            'phyl':random.randint(1, 1000)}, ignore_index=True)
 
-print(ws['A2'].value)
+data.to_excel('data.xlsx', index=False)
