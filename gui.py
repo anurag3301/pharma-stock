5d520da925b9eecb3f9337ebfbc886e9912e3021
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-01-11 16:36:21 +0530

    Started with black window

--- gui.py
+++ gui.py
@@ -1,32 +1,17 @@
 import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
 
-class MyWidget(QtWidgets.QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
-
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+        QMainWindow.__init__(self)
+        self.setWindowTitle("Tutorial")
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QApplication(sys.argv)
 
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
+    window = MainWindow()
+    window.resize(800, 600)
+    window.show()
 
     sys.exit(app.exec())
