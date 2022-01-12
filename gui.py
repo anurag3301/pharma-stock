21c2e0ba3abb7a7a702ae98d64cbf13f005fcc88
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2022-01-12 13:46:53 +0530

    Added search system in ui

--- gui.py
+++ gui.py
@@ -1,35 +1,72 @@
 import sys
+import pharma_data
 from PySide6.QtCore import Qt, Slot, QSortFilterProxyModel
 from PySide6.QtGui import QAction, Qt, QStandardItemModel, QStandardItem
 from PySide6.QtWidgets import (QApplication, QHeaderView, QHBoxLayout, QLabel, QLineEdit,
                                QMainWindow, QPushButton, QTableWidget, QTableWidgetItem,
-                               QVBoxLayout, QWidget)
+                               QVBoxLayout, QWidget, QTableView)
+class Widget(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        mainLayout = QVBoxLayout()
+        companies = pharma_data.data
+        model = QStandardItemModel(len(companies), 1)
+        model.setHorizontalHeaderLabels(['Company'])
+
+        for row, company in enumerate(companies):
+            item = QStandardItem(company)
+            model.setItem(row, 0, item)
+
+        filter_proxy_model = QSortFilterProxyModel()
+        filter_proxy_model.setSourceModel(model)
+        filter_proxy_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
+        filter_proxy_model.setFilterKeyColumn(0)
+
+        search_field = QLineEdit()          
+        search_field.setStyleSheet('font-size: 25px; height: 40px;')
+        search_field.textChanged.connect(filter_proxy_model.setFilterRegularExpression)
+        mainLayout.addWidget(search_field)
+
+        table = QTableView()
+        table.setStyleSheet('font-size: 20px;')
+        table.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        table.setModel(filter_proxy_model)
+        mainLayout.addWidget(table)
+
+        self.setLayout(mainLayout)
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, widget):
         QMainWindow.__init__(self)
-        self.setWindowTitle("Pharma-Stock")
+        self.setWindowTitle("Tutorial")
 
-        # menu bar
+        # Menu
         self.menu = self.menuBar()
         self.file_menu = self.menu.addMenu("File")
+
+        # Exit QAction
         exit_action = QAction("Exit", self)
         exit_action.setShortcut("Ctrl+Q")
         exit_action.triggered.connect(self.exit_app)
+
         self.file_menu.addAction(exit_action)
-
-        # left side
-
+        self.setCentralWidget(widget)
 
     @Slot()
     def exit_app(self, checked):
         QApplication.quit()
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
 
-    window = MainWindow()
-    window.resize(800, 600)
+if __name__ == "__main__":
+    # Qt Application
+    app = QApplication(sys.argv)
+    # QWidget
+    widget = Widget()
+    # QMainWindow using QWidget as central widget
+    window = MainWindow(widget)
+    window.resize(1200, 700)
     window.show()
 
+    # Execute application
     sys.exit(app.exec())
